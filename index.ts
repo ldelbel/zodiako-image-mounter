7a934be2aa64d770995f5828e6033e72f4aaac6e
Lucas Delbel <lucas@wisefy.io>
2025-04-28 15:45:09 -0300

    Update domain

--- index.ts
+++ index.ts
@@ -185,6 +185,6 @@ app.use((req, res) => {
 });
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
